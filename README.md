d8d5b261be2ba1dbad19ba03f784c8672179b428
drace <daren.race@gmail.com>
2020-01-22 21:06:13 -0800

    some suggested changes

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# streaming_word_tokenizer
-The goal of this project is to tokenzie 1GB file and get top n of n-grams within one minute.
-The first step is to write in python using generator to avoid text materialization. 
-The next step is to try to write in cpython and C to make it even faster.
+========================
+streaming_word_tokenizer
+========================
+
+GOALS
+-----
+
+The goal of this project is to tokenize text without having to read all of the test
+into memory. The projects aims to do this very efficiently with a target goal of
+1GB/minute on a 2020 MacBook Pro class of machine.
+
+The project uses streaming layers, the base layer emits token class objects that
+can be combined into n-grams if needed. This, the base layer, can be used in a
+program as a library given a sequence of source text files, or can be used in a
+CLI environment where the result will be displayed on stdout one token object per
+line.
+
+LICENSE
+-------
+
+New BSD. See LICENSE
+
+FUTURE
+------
+
+  - Evaluate performance improvements using cython
+  - TBD
+
+EXAMPLE
+-------
+
+DEPENDENCIES
+------------
+
+Supports Python 3.4+. It is pure Python and requires no dependencies beyond the
+standard library.
+
+RELATED PROJECTS:
+
+  - `TextBlob <https://github.com/sloria/TextBlob>`__`
---
+++ driver.py
@@ -0,0 +1,46 @@
+
+from streaming_tokenizer import StreamingTokenizer
+
+# NOTE: this should be turned into pytests
+
+def test_edge_cases():
+    text = "Shay's dog talked in Chinese, which is un-American"
+    for token in StreamingTokenizer(force_lower=True, emit_punctuation=True)\
+        .tokenize_str(text):
+        print(token)
+def test_text_string():
+    text = '''
+        It was the best of times,
+        it was the worst of times,
+        it was the age of wisdom,
+        it was the age of foolishness,
+        it was the epoch of belief,
+        it was the epoch of incredulity,
+        it was the season of Light,
+        it was the season of Darkness,
+        it was the spring of hope,
+        it was the winter of despair,
+        we had everything before us,
+        we had nothing before us,
+        we were all going direct to Heaven,
+        we were all going direct the other way--
+        in short, the period was so far like the present period, that some of
+        its noisiest authorities insisted on its being received, for good or for
+        evil, in the superlative degree of comparison only.
+    '''.strip()
+    for token in StreamingTokenizer(force_lower=True, emit_punctuation=True)\
+        .tokenize_str(text):
+        print(token)
+
+def test_dickens():
+    for token in StreamingTokenizer(force_lower=True, emit_punctuation=True)\
+        .tokenize_file('test_data/tale-of-two-cities.txt'):
+        print(token)
+
+def test_jkrowling():
+    for token in StreamingTokenizer(force_lower=True, emit_punctuation=True)\
+        .tokenize_file('test_data/the-philosophers-stone.txt'):
+        print(token)
+
+if __name__ == '__main__':
+    test_edge_cases()
