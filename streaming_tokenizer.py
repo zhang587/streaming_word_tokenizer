d129ed7e876eb06899d636517643a80743092827
Yingshi Zhang <yingshizhang@Yingshis-MacBook-Pro-2.local>
2020-01-26 17:57:36 -0800

    fix for io operation on closed file. yield from keeps the generator open

--- streaming_tokenizer.py
+++ streaming_tokenizer.py
@@ -40,8 +40,8 @@ class StreamingTokenizer(object):
         if not os.path.isfile(filepath):
             raise "File path {} does not exist. Exiting...".format(filepath)
         else:
-            fd = open(filepath, "r").readlines()
-            return self._tokenize(fd)
+            with open(filepath, "r", encoding = "utf-8") as fd:
+                yield from self._tokenize(fd)
 
     def _tokenize(self, fd):
         #
