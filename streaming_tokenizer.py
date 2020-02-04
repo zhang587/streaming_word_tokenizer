1a2a84a132af51ded9aa8e61ae54590b1911dd76
Yingshi Zhang <yingshizhang@Yingshis-MacBook-Pro-2.local>
2020-02-04 09:34:52 -0800

    omit punctuation, emit punctuation in progress

--- streaming_tokenizer.py
+++ streaming_tokenizer.py
@@ -5,24 +5,26 @@ from string import punctuation
 
 
 class Token(object):
-    def __init__(self, str, is_punctuation=False):
+    def __init__(self, str):
         self.str = str
-        self.is_punctuation = is_punctuation
-
+        self.is_punctuation = punctuation
         self.is_delimiter = not str.strip()
 
     def __str__(self):
         if self.is_delimiter:
             return '<DELIMITER>'
+        ## TODO: finish this part
+        if self.is_punctuation:
+            return '<PUNCTUATION>'
         else:
             return '<{}>'.format(self.str)
 
-
 class StreamingTokenizer(object):
 
-    def __init__(self, force_lower, emit_punctuation, split_pattern='\s+'):
+    def __init__(self, force_lower, omit_punctuation, emit_punctuation, split_pattern='\s+'):
         self.force_lower = force_lower
-        self.emit_punctuation = emit_punctuation # not used yet
+        self.omit_punctuation = omit_punctuation 
+        self.emit_punctuation = emit_punctuation
         # compile the split pattern for speed
         self.pattern = re.compile(split_pattern)
 
@@ -44,6 +46,7 @@ class StreamingTokenizer(object):
                 yield from self._tokenize(fd)
 
     def translate_str(self, word):
+        
         return word.translate(str.maketrans('', '', punctuation))
 
     def _tokenize(self, fd):
@@ -52,13 +55,16 @@ class StreamingTokenizer(object):
         # if this assumption doesn't hold the file must be read char by char
         #
         # Note: one way to detect punctuation is to use two patterns, one
-        # of the user provided strip patern and one of string.punctuation
+        # of the user provided strip pattern and one of string.punctuation
         # then let regex tell you which pattern matched, that's the most robust
         # way to detect punct chars (and runs of chars like "!!" as a single token)
         for line in fd:
             for word in self.pattern.split(line):
                 if self.force_lower:
                     word = word.lower()
-                if self.emit_punctuation:
+                if self.omit_punctuation:
                     word = self.translate_str(word)
+                ## TODO: finish this part
+                if self.emit_punctuation:
+                    char_list = [char for char in word]
                 yield Token(word)
